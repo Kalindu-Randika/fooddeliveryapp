2d35719ee0bc5f1ed68162406b88a5224b3fb712
Kalindu-Randika <kalindu.randika@gmail.com>
2023-09-30 20:40:11 +0530

    Initial commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import headerImage from "../../Assets/header.jpeg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = props => {
+    return(
+        <React.Fragment>
+            <header className={classes.header}>
+                <h1> Kalishu Meals</h1>
+               <HeaderCartButton />
+            </header>
+            <div className={classes['main-image']}>
+                <img src={headerImage}/>
+            </div>
+        </React.Fragment>
+    )
+
+
+}
+
+export default Header;
