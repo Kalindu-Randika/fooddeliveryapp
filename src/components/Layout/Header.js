59f612fd59cc8d2983341f184dcdb33e0215e86f
Kalindu-Randika <kalindu.randika@gmail.com>
2023-10-01 00:16:26 +0530

    Adding cart functionality

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = props => {
         <React.Fragment>
             <header className={classes.header}>
                 <h1> Kalishu Meals</h1>
-               <HeaderCartButton />
+               <HeaderCartButton onClick={props.onShowCart} />
             </header>
             <div className={classes['main-image']}>
                 <img src={headerImage}/>
