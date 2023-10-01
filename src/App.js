f4eb3240e8bf8214e13f3ddc6c3615778fa8970c
Kalindu-Randika <kalindu.randika@gmail.com>
2023-10-01 14:07:47 +0530

    Adding shopping cart update

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import { useState } from 'react';
 
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
-import Cart from "./components/Cart/Cart";
-import CartProvider from "./store/CartProvider";
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
-
     const [cartIsShown, setCartIsShown] = useState(false);
 
     const showCartHandler = () => {
-      setCartIsShown(true);
+        setCartIsShown(true);
     };
 
     const hideCartHandler = () => {
@@ -19,7 +18,7 @@ function App() {
 
     return (
         <CartProvider>
-            {cartIsShown && <Cart onClose={hideCartHandler}/>}
+            {cartIsShown && <Cart onClose={hideCartHandler} />}
             <Header onShowCart={showCartHandler} />
             <main>
                 <Meals />
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, {useContext} from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
 const HeaderCartButton = props => {
+    const cartCtx = useContext(CartContext);
+    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0);
+
     return (
         <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>
@@ -9,7 +15,7 @@ const HeaderCartButton = props => {
             </span>
             <span> Your Cart </span>
             <span className={classes.badge}>
-                3
+                {numberOfCartItems}
             </span>
         </button>
     )
